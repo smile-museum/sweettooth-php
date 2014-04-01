899da761347eccda16ff233894a921b25306f97a
Vlad Vanca <vlad@sweettoothhq.com>
2014-04-01 17:06:09 +0300

    Minor cosmetic update.

--- lib/SweetTooth/ChannelEnvironment.php
+++ lib/SweetTooth/ChannelEnvironment.php
@@ -3,12 +3,12 @@
  * Examples:
  *
  * SweetTooth_ChannelEnvironment::create(array(
- *   "url" => "http://example.com",
- *   "admin_url" => "http://example.com/admin",
- *   "connector_code" => "sweettooth-magento",
+ *   "url"               => "http://example.com",
+ *   "admin_url"         => "http://example.com/admin",
+ *   "connector_code"    => "sweettooth-magento",
  *   "connector_version" => "1.0.0",
- *   "extra" => array(
- *     "php_version" => "5.3",
+ *   "extra"             => array(
+ *     "php_version"     => "5.3",
  *     "magento_version" => "1.11.1.0",
  *     "magento_product" => "enterprise",
  *   )
@@ -23,7 +23,7 @@ class SweetTooth_ChannelEnvironment extends SweetTooth_ApiResource
 
   public static function classNamePlural($class)
   {
-    return "channel_environment";
+    return 'channel_environment';
   }
 
   public static function constructFrom($values, $apiKey=null)
