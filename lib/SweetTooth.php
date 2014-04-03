69080154a67f2d320b5924c8cb446d475116ae7f
Vlad Vanca <vlad@sweettoothhq.com>
2014-04-03 22:17:01 +0300

    Added ping resource. GET /v1/ping

--- lib/SweetTooth.php
+++ lib/SweetTooth.php
@@ -43,6 +43,7 @@ require(dirname(__FILE__) . '/SweetTooth/Spending.php');
 require(dirname(__FILE__) . '/SweetTooth/SpendingOption.php');
 require(dirname(__FILE__) . '/SweetTooth/PointsTransaction.php');
 require(dirname(__FILE__) . '/SweetTooth/ChannelEnvironment.php');
+require(dirname(__FILE__) . '/SweetTooth/Ping.php');
 
 // Deprecated Resources
 require(dirname(__FILE__) . '/SweetTooth/Event.php');
